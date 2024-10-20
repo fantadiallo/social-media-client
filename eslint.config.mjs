538a329b1fc756a7697bb47fc2725b51219675a8
fantadiallo <fatmbo0003@outlook.com>
2024-10-20 18:39:09 +0200

    Test commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,11 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    ignores: ['node_modules/**', 'dist/**'], 
+  },
   pluginJs.configs.recommended,
 ];
